ff1e3901ccbc111daaa6e2f8c1bb26f481034a70
Alessio Michelini <darkmavis1980@gmail.com>
2024-05-25 16:59:37 +0100

    feat: code improvements

--- handler.py
+++ handler.py
@@ -6,11 +6,23 @@ import requests
 Crawl the given URL and return the raw content, paragraphs and links
 """
 def crawl(event, context):
-    url = event['queryStringParameters']['url']
 
-    # # fetch the content from the url
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    query_string_params = event.get('queryStringParameters')
+    url = query_string_params.get('url') if query_string_params else None
+
+    if url is None:
+        return { "statusCode": 400, "body": json.dumps({"message": "Missing url parameter"})}
+    
+    # Try to fetch the content from the URL and catch any errors.
+    try:
+         
+        with requests.Session() as session:
+            page = session.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+    except Exception as e:
+        print(f"Error fetching {url}: {e}")
+        return {"statusCode": 400, "body": json.dumps({"error": str(e)})}
 
     raw_content = soup.get_text()
     paragraphs = soup.find_all('p')
